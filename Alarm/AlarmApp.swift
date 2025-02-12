c479d85c62002ee8970f11462cd31e28e456f71e
fbara <fbara1@yahoo.com>
2025-02-12 12:07:26 -0500

    Initial Commit

---
+++ Alarm/AlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmApp.swift
+//  Alarm
+//
+//  Created by Frank Bara on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct AlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
