682259349fa57ad3758df965f9d276fa9397e579
fbara <fbara1@yahoo.com>
2025-02-14 15:48:16 -0500

    Added SettingsView.

---
+++ Alarm/Views/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Alarm
+//
+//  Created by Frank Bara on 2/14/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
